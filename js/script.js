58ea0f1735fee88b11bb201dfafae558698afbe7
Nikita Hovratov <nikita.h@live.de>
2018-04-15 19:31:59 +0200

    Add css class depending on change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        history: [
+            {points: 2621, date: '', change: 0},
+            {points: 2601, change: -20, date: ''},
+            {points: 2624, change: 23, date: ''},
+            {points: 2599, change: -25, date: ''}
+        ]
+    },
+    methods: {
+        changeClass: function (item) {
+            return {
+                'text-success': item.change > 0,
+                'text-danger': item.change < 0,
+                'text-warning': item.change === 0
+            }
+        }
     }
 });
